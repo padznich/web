671a59c17385a91d9b2d73f00066eb21b999a25e
padznich <slabkopg@gmail.com>
2016-05-16 16:03:41 +0300

    ask

---
+++ ask/qa/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+
+
+def test(request, *args, **kwargs):
+
+    return HttpResponse('OK')
--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart﻿﻿
